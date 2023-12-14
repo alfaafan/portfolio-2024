89f20938009f9ca1eb46eedc045514ce84602292
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-12-14 21:30:24 +0700

    create navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl">Alfaafan Yusufa Achmad</h1>
+      <button className="btn">Click me</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import HamburgerButton from "./HamburgerButton";
+import "./Navbar.css";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleTapChange = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-base-100">
+      <div className="navbar border-red-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">Alfaafan</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1 hidden sm:flex">
+            <li>
+              <a>Profile</a>
+            </li>
+            <li>
+              <a>Stacks</a>
+            </li>
+            <li>
+              <a>Works</a>
+            </li>
+          </ul>
+          <HamburgerButton isOpen={isOpen} onTapChange={handleTapChange} />
+        </div>
+      </div>
+      {isOpen ? (
+        <>
+          <ul className="sm:hidden ps-5 py-2">
+            <li className="pt-5">
+              <a href="#profile" className="btn btn-ghost">
+                Profile
+              </a>
+            </li>
+            <li className="pt-5">
+              <a href="#stacks" className="btn btn-ghost">
+                Stacks
+              </a>
+            </li>
+            <li className="pt-5">
+              <a href="#works" className="btn btn-ghost">
+                Works
+              </a>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <></>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
